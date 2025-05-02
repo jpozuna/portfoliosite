07ec26cac15fa34c1735f1bfa26cffcc2a4f465e
jpozuna <paulinoozuna.j@northeastern.edu>
2025-05-02 00:33:11 -0400

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="content">
+        <div className="black-square">
+          <div className="square-content">
+            <h1 className="name">YOUR NAME</h1>
+            <p className="title">SOFTWARE DEVELOPER</p>
+          </div>
+        </div>
+        <nav className="navigation">
+          <a href="#about">ABOUT</a>
+          <a href="#projects">PROJECTS</a>
+          <a href="#contact">CONTACT</a>
+        </nav>
+      </div>
     </div>
   );
 }
